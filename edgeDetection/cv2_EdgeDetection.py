26e1bc76a1861e5096629500973d59ec3cba1d07
tsal4 <159286965+tsal4@users.noreply.github.com>
2025-04-26 18:01:54 -0400

    changed some comments and small stuff in both edge detection files

--- edgeDetection/cv2_EdgeDetection.py
+++ edgeDetection/cv2_EdgeDetection.py
@@ -8,8 +8,11 @@ Created on Mon Apr  7 20:32:37 2025
 import cv2
 import numpy as np
 
+#import image
 image = cv2.imread('image2.jpg')
 
-edges = cv2.Canny(image, 75, 155)
+#transform image
+edges = cv2.Canny(image, 75, 155) #thresholds between 0-255
 
-cv2.imwrite("cv2CannyOutput.jpg", edges)
+#save image
+cv2.imwrite("cv2_output.jpg", edges)
--- edgeDetection/hedModel_EdgeDetection.py
+++ edgeDetection/hedModel_EdgeDetection.py
@@ -7,28 +7,27 @@ Created on Mon Apr  7 20:08:07 2025
 
 import cv2
 
-# Load the network
+#set up the model
 net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'hed_pretrained_bsds.caffemodel')
 
-# Read the image
+#import image
 image = cv2.imread('image2.jpg')
 (H, W) = image.shape[:2]
 
-# Prepare the image as input to the network
+#use the model to transform the image
 blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size=(W, H),
                              mean=(104.00698793, 116.66876762, 122.67891434),
                              swapRB=False, crop=False)
 
-# Set the blob as input to the network and perform a forward pass
 net.setInput(blob)
 hed = net.forward()
 hed = hed[0, 0]
 hed = cv2.resize(hed, (W, H))
-hed = (255 * hed).astype("uint8")
+hed = (255 * hed).astype("uint8") #output
 
-# Apply binary threshold to get cleaner, high-contrast edges
-threshold_value = 30  # You can tweak this (try 30–100)
-_, edge_thresh = cv2.threshold(hed, threshold_value, 255, cv2.THRESH_BINARY)
+#apply threshold (optional)
+#threshold_value = 30  #between 30-100
+#_, edge_thresh = cv2.threshold(hed, threshold_value, 255, cv2.THRESH_BINARY)
 
-# Save or display the result
-cv2.imwrite("hedModelOutput.jpg", edge_thresh)
+#save output
+cv2.imwrite("hed_model_output.jpg", hed)
